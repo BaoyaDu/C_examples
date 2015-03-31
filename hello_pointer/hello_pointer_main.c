8463eb464f675b570bbeae79b5e5c84e0f73966b
Baoya Du <279899228@qq.com>
2015-03-31 10:51:23 +0800

    Add package hello_pointer

---
+++ hello_pointer/hello_pointer_main.c
@@ -0,0 +1,17 @@
+/**
+* The main program of hello_pointer package.
+* Created on March 25,2015 Wed.
+*/
+
+#include <stdio.h>
+
+int main(int argc,char *argv[])
+{
+	printf("-- Hello,Pointer\n");
+	
+	simple_pointers();
+	
+	
+	return 0;
+}
+
